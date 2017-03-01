c24e617297e17bf35841acbaf8ad253a8e66f2e7
LIJING <lijinging@outlook.com>
2017-03-01 10:41:20 +0800

    添加背景颜色设置

--- CreateImage.py
+++ CreateImage.py
@@ -1,16 +1,14 @@
-def createImg(width=1920, height=1080, colors=[(255,255,255),(0,0,0)], filename="img.png", type="png"):
+def createImg(width=1920, height=1080, back=(255,255,255), colors=[(255,255,255), (0,0,0)], filename="img.png", type="png"):
     import Image
     import ImageDraw
-    image = Image.new('RGB', (width, 2160), (255, 255, 255))
+    image = Image.new('RGB', (width, height), back)
     draw = ImageDraw.Draw(image)
     length = len(colors)
     for y in range(height):
         index = y % length
-
         for x in range(width):
             draw.point((x, y), fill=colors[index])
             index = (index + 1) % length
-
     image.save(filename, type)
 
 
